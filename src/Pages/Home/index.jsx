86ebd14982782ef2365384fd205a5b9458925fbe
ducanh2710 <anhpham271003@gmail.com>
2025-08-19 14:00:29 +0700

    category slider

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Slider from '../../components/Slider';
+import CategorySlider from '../../components/CategorySlider';
 
 const Home  = () => {
     return (
         <>
         <div className ="w-full">  
             <Slider/>
+            <CategorySlider/>
         Home</div>
         </>
     )
