143049290b089696e498bb9be4917424cec1c6a3
ducanh2710 <anhpham271003@gmail.com>
2025-08-20 11:48:18 +0700

    add ads banner slider

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import Slider from '../../components/Slider';
-import CategorySlider from '../../components/CategorySlider';
+import React from "react";
+import Slider from "../../components/Slider";
+import CategorySlider from "../../components/CategorySlider";
+import BannerSlider from "../../components/BannerSlider";
 import { FaShippingFast } from "react-icons/fa";
 
-const Home  = () => {
-    return (
-        <>
-            <Slider/>
-            <CategorySlider/>
-        
-            <section className = "py-15 bg-white">
-                <div className="container">
-                    <div className="freeShipping w-full p-4  border-4 border-amber-500 rounded-md flex items-center justify-between">
-                        <div className="col-1 flex items-center gap-4">
-                            <FaShippingFast className ="text-[50px]"></FaShippingFast>
-                            <span className = "text-[20px] font-bold">FREE SHIPPING</span>
-                        </div>
+const Home = () => {
+  return (
+    <>
+      <Slider />
+      <CategorySlider />
 
-                        <div className="col-2 !px-[100px] border-x-3 border-x-gray-400 " >
-                            <p className =" font-liber text-[23px]  ">  Free Delivery on all orders over $500</p>
-                        </div>
+      <section className=" py-15 bg-white">
+        <div className="container">
 
-                        <div className="col-3">
-                            <p className ="font-bold text-[25px]">- Only $500*</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <br /> <br /> <br /> <br /> <br /> <br />
-        </>
-    )
-}
+            {/* Free Shipping Banner */}    
+          <div className="freeShipping !w-[90%] !m-auto p-4  border-4 border-amber-500 rounded-md flex items-center justify-between">
+            <div className="col-1 flex items-center gap-4">
+              <FaShippingFast className="text-[50px]"></FaShippingFast>
+              <span className="text-[20px] font-bold">FREE SHIPPING</span>
+            </div>
+
+            <div className="col-2 !px-[100px] border-x-3 border-x-gray-400 ">
+              <p className=" font-liber text-[23px]  ">
+                {" "}
+                Free Delivery on all orders over $500
+              </p>
+            </div>
+
+            <div className="col-3">
+              <p className="font-bold text-[25px]">- Only $500*</p>
+            </div>
+          </div>
+
+            {/* //adsBanner */}
+          <BannerSlider items={3} />
+        </div>
+      </section>
+      <br /> <br /> <br /> <br /> <br /> <br />
+    </>
+  );
+};
 
 export default Home;
---
+++ src/components/BannerSlider/index.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./BannerSlider.css";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
+
+const BannerSlider = (props) => {
+  return (
+    <div className="w-full">
+      <Swiper
+        slidesPerView={props.items}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        slidesOffsetBefore={0}
+        className="mySwiper "
+      >
+        <SwiperSlide>
+          <Link to="/">
+            <div className="bannerBox rounded-xl overflow-hidden">
+              <img
+                src="https://cdn.s99.vn/cloud/Banner-quang-cao-dien-thoai-thong-minh-file-PSD-mau-HB3-24122024.jpg"
+                alt="banner1"
+                className="w-full cover transition-all " 
+              />
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to="/">
+            <div className="bannerBox rounded-xl overflow-hidden">
+              <img
+                src="https://cdn.s99.vn/cloud/Banner-quang-cao-dien-thoai-thong-minh-file-vector-mau-JH273-03032025.jpg"
+                alt="banner1"
+                className="w-full cover transition-all "
+              />
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to="/">
+            <div className="bannerBox rounded-xl overflow-hidden">
+              <img
+                src="https://prbaochi.com/wp-content/uploads/2021/07/mau-banner-quang-cao-san-pham-15.jpg"
+                alt="banner1"
+                className="w-full cover transition-all"
+              />
+            </div>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to="/">
+            <div className="bannerBox rounded-xl overflow-hidden">
+              <img
+                src="https://logo.sibic.vn/upload/image/20231202/6383710935847606737329161.jpg"
+                alt="banner1"
+                className="w-full cover transition-all"
+              />
+            </div>
+          </Link>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BannerSlider;
