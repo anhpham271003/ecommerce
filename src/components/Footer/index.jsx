001f1a8d2127f2da3a8695f8ac076d736fdb395b
ducanh2710 <anhpham271003@gmail.com>
2025-08-22 15:13:45 +0700

    add button contact

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { IoChatbubblesOutline } from "react-icons/io5";
 
+import { FaFacebookMessenger, FaFacebook, FaPhone } from "react-icons/fa";
+import { SiZalo } from "react-icons/si";
+
 const Footer = () => {
   return (
     <div className="bg-white flex py-10">
@@ -110,14 +113,47 @@ const Footer = () => {
       {/* FooterRight */}
       <div className="rightPart w-[35%] !my-0">
         <h2 className="text-[20px] font-[600] !mb-6 ">Site Map</h2>
-            <iframe
+        <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233.05388683454643!2d106.64560072124003!3d20.83727071146793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7107a9c1e5ad%3A0xec7bac66bc65f06f!2sHo%C3%A0ng%20mai!5e0!3m2!1sen!2s!4v1755848353289!5m2!1sen!2s"
-         className="w-[100%] h-[90%]"
-           style={{ border: 0 }}
+          className="w-[100%] h-[90%]"
+          style={{ border: 0 }}
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
+      </div>
+
+      {/* //contact */}
+      <div className="fixed bottom-5 left-5 flex flex-col !gap-2 z-50">
+        {/* Messenger */}
+        <div>
+          <Link
+            to="https://m.me/phamducanh271003"
+            className="w-10 h-10 flex items-center justify-center bg-blue-500 text-white rounded-full shadow-lg hover:scale-110 transition-all opacity-90"
+          >
+            <FaFacebookMessenger size={24} />
+          </Link>
+        </div>
+
+        {/* Facebook */}
+        <div>
+          <Link
+            to="https://www.facebook.com/phamducanh271003"
+            className="w-10 h-10 flex items-center justify-center bg-blue-600 text-white rounded-full shadow-lg hover:scale-110 transition-all opacity-90"
+          >
+            <FaFacebook size={24} />
+          </Link>
+        </div>
+
+        {/* Zalo */}
+        <div>
+          <Link
+            to="https://zalo.me/0362025195"
+            className="w-10 h-10 flex items-center justify-center bg-cyan-500 text-white rounded-full shadow-lg hover:scale-110 transition-all opacity-90"
+          >
+            <SiZalo size={24} />
+          </Link>
+        </div>
       </div>
     </div>
   );
