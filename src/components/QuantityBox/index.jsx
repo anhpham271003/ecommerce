61f1830f708d318afe89c7211f0a0069e0eb9554
ducanh2710 <anhpham271003@gmail.com>
2025-08-27 22:46:07 +0700

    add control up dow input and fix width button

--- src/components/QuantityBox/index.jsx
+++ src/components/QuantityBox/index.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./QuantityBox.css";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import Button from "@mui/material/Button";
 import { FiMinusCircle } from "react-icons/fi";
 
 const QuantityBox = () => {
+  const [qty, setQty] = useState(1);
+
+  const increase = () => setQty(qty + 1);
+  const decrease = () => {
+    if (qty > 1) setQty(qty - 1);
+  };
+
   return (
     <div className="qtyBox flex items-center">
-      <Button className="!w-[40px] !h-[40px] !rounded-full !p-0 !text-black">
-        <IoMdAddCircleOutline className="text-[30px] !m-0 text-[rgba(0,0,0,0.6)]" />
+      <Button
+        className="!rounded-full !p-0 flex items-center justify-center !min-w-[35px] !text-black"
+        onClick={decrease}
+      >
+        <FiMinusCircle className="text-[30px] !m-0 text-[rgba(0,0,0,0.6)]" />
       </Button>
 
       <input
         type="number"
+        value={qty}
         min="1"
         className="w-[50px] h-[40px] p-2 text-center text-[15px] focus:outline-none border border-[rgba(0,0,0,0.2)] rounded-md"
       />
 
-      <Button className="!w-[40px] !h-[40px] !rounded-full !p-0 !text-black">
-        <FiMinusCircle className="text-[30px] !m-0 text-[rgba(0,0,0,0.6)]" />
+      <Button
+        className="!rounded-full !p-0 flex items-center justify-center !min-w-[35px] !text-black"
+        onClick={increase}
+      >
+        <IoMdAddCircleOutline className="text-[30px] !m-0 text-[rgba(0,0,0,0.6)]" />
       </Button>
     </div>
   );
